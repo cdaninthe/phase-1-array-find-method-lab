b2d3e434551a76263aa97a0a1baac7ed0b23d38a
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2022-01-06 18:40:53 -0500

    commit tree 5dd879a8d582700134b9a52f73b6816cf51d07a0

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+//function superbowlWin(record, index){
+//    function year(record, index){
+//      return record[index].year;
+//    }
+//    if (record[index].result === 'W') {
+//      return year(record, index);
+//    }
+//    else {
+//        return 'undefined';
+//    }
+//  }
+
+//function superbowlWin(record){
+//    const win = record.find( record => record.result === 'W')
+//    if (win.result === 'W'){
+ //     return win.year;
+ //   } 
+ //   else {
+//      return undefined;
+//    }
+//}
+
+function superbowlWin(record){
+    const win = record.find( record => record.result === 'W')
+    console.log(win)
+    if (win){
+        return win.year;
+    }
+}
